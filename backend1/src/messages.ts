9418714de2d781441b269e669528df668a4a4967
Rudra-IITM <singhrudra5556@gmail.com>
2024-04-21 00:12:05 +0530

    restructed folder

--- backend1/src/messages.ts
+++
@@ -1,5 +0,0 @@
-export const INIT_GAME = 'init_game';
-export const MOVE = 'move';
-export const DEFAULT_POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-export const GAME_OVER = 'game_over';
-export const GAME_STARTED = 'game_started';
